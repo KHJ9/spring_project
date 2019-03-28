457772a31f56f671054a6b155ca97692e374dea3
flower9 <flowerdisk9@naver.com>
2019-03-28 21:42:13 +0900

    Mybatis Controller added (Practice)

--- Spring_project3/src/main/java/com/dbtest/controller/DBController.java
+++ Spring_project3/src/main/java/com/dbtest/controller/DBController.java
@@ -1,17 +1,38 @@
 package com.dbtest.controller;
 
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dbtest.dao.Mapper;
 
 @Controller
 public class DBController {
 
-	@RequestMapping("/Mybatis")
-	public String mybatis_page () {
+	private static final Logger logger = (Logger) LoggerFactory.getLogger(DBController.class);
+	
+	@Autowired
+	private Mapper mapper;
+	
+	@RequestMapping(method=RequestMethod.POST, value="/Mybatis")
+	public String list(HttpServletRequest request, Model model) {
 		
+		String register = request.getParameter("register");
 		
-		
-		return null;
+		model.addAttribute("register", mapper.primary_num("register"));
+		return "Mybatis_practice";
 	}
 	
 }
+
+
+
+
+
